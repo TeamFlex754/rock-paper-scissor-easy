0a0b5dcc0e8635879c3359939bc73d98fc271404
Ian McVay <ians.mcvay@gmail.com>
2025-03-30 12:49:11 -0700

    Add files via upload
    third commit

--- main.py
+++ main.py
@@ -1,52 +1,44 @@
-# print("...rock...")
-# print("...paper...")
-# print("...scissors...")
-# player1_choice = input("(enter Player 1's choice): ").lower()
-# player2_choice = input("(enter Player 2's choice): ").lower()
-# player1 = "Player 1"
-# player2 = "Player 2"
-# winner = None
-#
-# if player1_choice == "rock" and player2_choice == "scissors":
-#     winner = player1
-# elif player1_choice == "paper" and player2_choice == "rock":
-#     winner = player1
-# elif player1_choice == "scissors" and player2_choice == "paper":
-#     winner = player1
-# else:
-#     winner = player2
-#
-# print(f"{winner} wins!")
-
 from random import randint
 
-player = input("Player 1, make your move: ").lower()
-rand_num = randint(0,2)
+print("....rock.....")
+print("....paper.....")
+print("....scissors....\n")
 
-if rand_num == 0:
-    computer = "rock"
-elif rand_num == 1:
-    computer = "paper"
-else:
-    computer = "scissors"
-print(f"Computer picks {computer}.")
+while True:
 
-if player == computer:
-    print("It's a tie!")
-elif player == "rock":
-    if computer == "scissors":
-        print("Player wins!")
-    elif computer == "paper":
-        print("Computer wins!")
-elif player == "paper":
-    if computer == "rock":
-        print("Player wins!")
-    elif computer == "scissors":
-        print("Computer wins!")
-elif player == "scissors":
-    if computer == "paper":
-        print("Player wins!")
-    elif computer == "rock":
-        print("Computer wins!")
-else:
-    print("Something went wrong!")
+    player = input("Player 1, make your move: ").lower()
+    rand_num = randint(0,2)
+
+    if rand_num == 0:
+        computer = "rock"
+    elif rand_num == 1:
+        computer = "paper"
+    else:
+        computer = "scissors"
+    print(f"Computer picks {computer}.")
+
+    if player == computer:
+        print("It's a tie!")
+    elif player == "rock":
+        if computer == "scissors":
+            print("Player wins!\n")
+        elif computer == "paper":
+            print("Computer wins!\n")
+    elif player == "paper":
+        if computer == "rock":
+            print("Player wins!\n")
+        elif computer == "scissors":
+            print("Computer wins!\n")
+    elif player == "scissors":
+        if computer == "paper":
+            print("Player wins!\n")
+        elif computer == "rock":
+            print("Computer wins!\n")
+    else:
+        print("Something went wrong!")
+
+    continue_game = input("Play again? (y/n): ").lower()
+    if continue_game != "y":
+        print("Thanks for playing!")
+        break
+
